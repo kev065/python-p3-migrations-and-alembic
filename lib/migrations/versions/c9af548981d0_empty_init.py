0e1f1569c8afb83888b7cf4e8f8d54724b79954b
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-06 12:26:49 +0300

    file created

---
+++ lib/migrations/versions/c9af548981d0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c9af548981d0
+Revises: 
+Create Date: 2023-12-06 12:24:33.976103
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c9af548981d0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
